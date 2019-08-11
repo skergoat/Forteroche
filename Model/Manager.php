f7a8a77d4babf7ed714fa59cfe12325418a5b3d7
Stephane <apsarawebdev@protonmail.com>
2019-08-11 18:22:23 +0800

    manager

--- Model/Manager.php
+++ Model/Manager.php
@@ -11,7 +11,7 @@ class Manager
 
 	public function __construct()
 	{
-		$this->bdd = new \PDO('mysql:host=concombre.o2switch.net;dbname=skergoat_forteroche;charset=utf8', 'skergoat', '919528565x9?securite', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));   
+		$this->bdd = new \PDO('mysql:host=concombre.o2switch.net;dbname=skergoat_forteroche;charset=utf8', '', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));   
 	} 
 
 	public function getManager() {
